2185b3f6ac98dac722ea982399b97763126d24a7
lwear <33907570+lwear@users.noreply.github.com>
2024-10-02 13:44:27 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,15 +5,15 @@ self.addEventListener('install', function(event) {
     caches.open('my-cache').then(function(cache) {
       return cache.addAll([
         '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/icons/horse-192-192.png',
-        '/icons/horse-48-48.png',
-        '/icons/horse-72-72.png',
-        '/icons/horse-96-96.png',
-        '/icons/horse-144-144.png',
-        '/icons/horse-512-512.png'
+        'index.html',
+        'style.css',
+        'script.js',
+        'icons/horse-192-192.png',
+        'icons/horse-48-48.png',
+        'icons/horse-72-72.png',
+        'icons/horse-96-96.png',
+        'icons/horse-144-144.png',
+        'icons/horse-512-512.png'
       ]);
     })
   );
