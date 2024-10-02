7b81ea6c96290b7843f8b8a5bec2238dc35c6467
lwear <33907570+lwear@users.noreply.github.com>
2024-10-02 13:34:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // install service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('pwa-test/sw.js').then(function(registration) {
       console.log('Service Worker registered with scope:', registration.scope);
     }, function(error) {
       console.log('Service Worker registration failed:', error);
