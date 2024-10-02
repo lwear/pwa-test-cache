9e12509cefa5f95c425d1e8e5c078ea502809984
lwear <33907570+lwear@users.noreply.github.com>
2024-10-02 13:50:50 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const version = "v338";
+const version = "v339";
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('my-cache').then(function(cache) {
       return cache.addAll([
-        '/'       
+        'index.html'       
       ]);
     })
   );
