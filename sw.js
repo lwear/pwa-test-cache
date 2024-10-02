acde61a1b7d4bec7efd1ad279f23b70795c6f48e
lwear <33907570+lwear@users.noreply.github.com>
2024-10-02 13:53:58 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,18 @@
-const version = "v339";
+const version = "v340";
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('my-cache').then(function(cache) {
       return cache.addAll([
-        'index.html'       
+        'index.html',
+        'style.css',
+        'script.js',
+        'icons/horse-192-192.png',
+        'icons/horse-48-48.png',
+        'icons/horse-72-72.png',
+        'icons/horse-96-96.png',
+        'icons/horse-144-144.png',
+        'icons/horse-512-512.png'
       ]);
     })
   );
