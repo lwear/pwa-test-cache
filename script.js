b0d5d60634273567af8d880e27196d54ac82c7ab
lwear <33907570+lwear@users.noreply.github.com>
2024-10-03 14:55:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/sw.js')
       .then(registration => {
         console.log('ServiceWorker registration successful with scope:', registration.scope);
       })
